d9d6f167849506fceb1c3e2efc797ff31fc00d71
RoChuquiana7845 <rchuquianat@unemi.edu.ec>
2023-06-23 10:20:33 -0500

    Deber finalizado

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -7,7 +7,7 @@ class Player {
         this.namedeck = namedeck;
         this.apiUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'; 
         this.points = 0;
-        this.gamesWon = 0;
+        this.gamesWon = 0;      
         this.deck = "";
 
     }
@@ -22,12 +22,17 @@ class Player {
                     <video src="../video/hey ya! pero en 8bit.mp4" width="300" height="350" controls autoplay loop></video>
             </div>
             `,
+            title: 'Ingrese el nombre del jugador',
+            input: 'text',
             confirmButtonText: 'Comenzar',
             width: '90%',
+            height: '80%',
             background: '#000',
             color: '#fff'
-        });
-        this.name.innerHTML = prompt("Ingrese el nombre del jugador 1");
+        })
+        .then((resultado)=>{ 
+            this.name.innerHTML = resultado.value;
+        })
     }
 
     showDeck () { 
@@ -48,14 +53,22 @@ class Player {
         this.sumOfCards.textContent = this.points;
     }
 
-    checkRoundLooser() { 
-        if (this.points>= 21) { 
-            alert('Perdiste');
-        }
-    }
-
-    pointForRoundVictory(mensaje) { 
+    pointForRoundVictory(mensaje, estadisticas) { 
         this.gamesWon += 1;
+        Swal.fire({ 
+            html: `
+            <div style:'border-radius:5%; font-family: 'Times New Roman';'>
+                <h2>${mensaje}</h2>
+                <iframe width="350" height="500" src="https://www.youtube.com/embed/OyHImE9hQmQ" title="John Cena  Dancing Tiktok Mem original" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <p>Estadisticas:</p><br>
+                <p><span>${estadisticas}</span></p>
+            </div>
+            `,
+            width: '500px',
+            height: '500px',
+            background: '#000',
+            color: '#fff'    
+        })
         alert(mensaje);
         this.score.innerHTML = this.gamesWon;
     }
@@ -65,7 +78,7 @@ class Player {
             const response = await fetch('https://deckofcardsapi.com/api/deck/' + this.deck + '/draw/?count=1');
             const result = await response.json();
             let div = document.createElement('div'); 
-            div.setAttribute('id', 'cartas-jugador'); 
+            div.setAttribute('id', 'carta-jugador'); 
             div.setAttribute('class', 'carta');
             div.innerHTML += `<img class="carta-img flip-in-ver-right" src="${result.cards[0].image}" alt="Carta: ${result.cards[0].value} ${result.cards[0].suit}">`;
             this.divCards.appendChild(div);
@@ -89,6 +102,29 @@ class Player {
     }
 
 }
+
+class UserPLayer extends Player { 
+    checkRoundLooser() { 
+        if (this.points> 21) { 
+            Swal.fire({
+                html: `
+                <div style:'border-radius:5%; font-family: 'Times New Roman';'>
+                    <h2>Perdiste 😭😭</h2>
+                    <iframe width="500" height="315" src="https://www.youtube.com/embed/xaZfsypesSs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                `,
+                width: '500px',
+                height: '500px',
+                background: '#000',
+                color: '#fff'
+            })
+            let points =document.querySelector('#jugador2puntos').innerHTML;
+            points++;
+            document.getElementById('jugador2puntos').innerText=points;
+        }
+    }
+}
+
 class MachinePlayer extends Player { 
     play() {
         let probabilidad = Math.floor(Math.random()*4);
@@ -103,11 +139,41 @@ class MachinePlayer extends Player {
         
     }
 
+    checkRoundLooser() { 
+        if (this.points> 21) { 
+            Swal.fire({
+                html: `
+                <div style:'border-radius:5%; font-family: 'Times New Roman';'>
+                    <h2>Perdio la AI 😭🤖😭</h2>
+                    <iframe width="500" height="315" src="https://www.youtube.com/embed/xaZfsypesSs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                `,
+                width: '500px',
+                height: '500px',
+                background: '#000',
+                color: '#fff'
+            });
+            let points =document.querySelector('#jugador1puntos').innerHTML;
+            points++;
+            document.getElementById('jugador1puntos').innerText=points;
+        }
+    }
+
     mechanismPlayer(level) { 
         if(this.points <= level) { 
             this.playUser();
         } else { 
-            alert("Paso🥸");
+            Swal.fire({
+                html: `
+                <h2>Paso🥸</h2>
+                <div style='position:relative; padding-bottom:calc(57.50% + 44px)'><iframe src='https://gfycat.com/ifr/LawfulInformalAntelopegroundsquirrel' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen></iframe></div>`,
+                confirmButtonText: `Aceptar que la AI es mas inteligente :v`,
+                width: '500px',
+                height: '500px',
+                background: '#000',
+                color: '#fff'
+
+            })
         }
     }
 }
@@ -164,11 +230,22 @@ class Game {
             alert("Empate");
         } else if (this.player1.points >= 21 || this.player2.points >=21) {
             if (this.player1.points == 21 || this.player1.points <= this.player2.points) { 
-                this.player1.pointForRoundVictory(`Ganaste 🤗: PuntajeUsuario: ${this.player1.points}, PuntajeMaquina: ${this.player2.points}`);
+                this.player1.pointForRoundVictory(`Ganaste 🤗`, `PuntajeUsuario: ${this.player1.points} vs PuntajeMaquina: ${this.player2.points}`);
             } else if(this.player2.points == 21 || this.player2.points <= this.player1.points) { 
-                this.player2.pointForRoundVictory(`Ganó la IA 🤖: PuntajeUsuario: ${this.player1.points}, PuntajeMaquina: ${this.player2.points}`)
+                this.player2.pointForRoundVictory(`Ganó la IA 🤖`, `PuntajeUsuario: ${this.player1.points}vs PuntajeMaquina: ${this.player2.points}`)
             } else { 
-                alert('Empate');
+                Swal.fire({
+                    html: `
+                    <div style:'border-radius:5%; font-family: 'Times New Roman';'>
+                        <h2>Empate 🤓🤓🤓</h2>
+                        <iframe width="500" height="285" src="https://www.youtube.com/embed/I4xwCT1F5RA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                    `,
+                    width: '500px',
+                    height: '500px',
+                    background: '#000',
+                    color: '#fff'
+                })
             }
         }
     }
@@ -179,9 +256,10 @@ class Game {
             this.player2.points = 0;
             this.player1.sumOfCards.textContent = 0;
             this.player2.sumOfCards.textContent = 0;
-            document.querySelectorAll("#cartas-jugador").forEach((card)=>{
+            document.querySelectorAll("#carta-jugador").forEach((card)=>{
                 card.remove();
-            })
+            });
+            this.startCardsOfPlayers();
         })
     }
 
@@ -194,7 +272,7 @@ class Game {
      
 }
 
-const playeruser = new Player(document.querySelector("#jugador1"), document.querySelector("#jugador1puntos"), document.querySelector("#cartas-jugador"), document.querySelector("#puntajejugador"), document.querySelectorAll("#text-mazo"));
+const playeruser = new UserPLayer(document.querySelector("#jugador1"), document.querySelector("#jugador1puntos"), document.querySelector("#cartas-jugador"), document.querySelector("#puntajejugador"), document.querySelectorAll("#text-mazo"));
 const playermachine = new MachinePlayer(document.querySelector("#jugador2"), document.querySelector("#jugador2puntos"), document.querySelector("#cartas-maquina"), document.querySelector("#puntajemaquina") , document.querySelectorAll("#text-mazo"));
 const game1 = new Game(playeruser, playermachine, document.querySelector("#solicitar-carta"), document.querySelector("#plantarse"), document.querySelector("#finalizar"), document.querySelector("#reiniciar")); 
 game1.starGame();
